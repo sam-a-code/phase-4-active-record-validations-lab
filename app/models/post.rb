9a69ce5f5575ba1a669028d254c78236b9475723
Sam Avery <sam.avery1@gmail.com>
2022-11-01 17:06:26 -0500

    commit tree bc80c0e6023b7fbe60503733442fb5f11acf4d2f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+  validates :title, exclusion: {in: ["True Facts"]}
+
+
+
+
+
+
+
+
+
+  # validates :category, {"Fiction" || "Non-Fiction"}
+
+
+  #why didn't this work?
+  # validates :title, exclusion: {"True Facts"}
+
+
 end
